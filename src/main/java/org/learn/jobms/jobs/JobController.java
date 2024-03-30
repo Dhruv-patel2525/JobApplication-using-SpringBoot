962507a4636e99dd3b8f2880295eae0d64bd8690
neha-code19 <53231876+neha-code19@users.noreply.github.com>
2024-03-30 18:37:03 -0400

    job microservices

---
+++ src/main/java/org/learn/jobms/jobs/JobController.java
@@ -0,0 +1,66 @@
+package org.learn.jobms.jobs;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+
+    private JobService jobService;
+
+    public JobController(JobService jobService)
+    {
+        this.jobService = jobService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Job>> findAll()
+    {
+        return ResponseEntity.ok(jobService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createJob(@RequestBody Job job)
+    {
+        jobService.createJob(job);
+        return new ResponseEntity<>("Job Added Successfully",HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable Long id)
+    {
+        Job job = jobService.getJobById(id);
+        if(job != null)
+        {
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteJobById(@PathVariable Long id)
+    {
+        boolean isDeleted = jobService.deleteJobById(id);
+        if(isDeleted)
+        {
+            return ResponseEntity.ok("Job Deleted Successfully");
+        }
+        return new ResponseEntity<>("Job Not Found!",HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateJobById(@PathVariable Long id,@RequestBody Job job)
+    {
+        boolean isUpdated = jobService.updateJobById(id,job);
+        if(isUpdated)
+        {
+            return ResponseEntity.ok("Updated SuccessFully");
+        }
+        return new ResponseEntity<>("Job id not found",HttpStatus.NOT_FOUND);
+    }
+
+}
---
+++ src/main/java/org/learn/jobms/jobs/JobRepository.java
@@ -0,0 +1,8 @@
+package org.learn.jobms.jobs;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Job,Long>
+{
+
+}
